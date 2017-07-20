4f222a1217357186e97235448e29efda15e31a4b
busy_boy <1538302885@qq.com>
2017-07-21 01:56:39 +0800

    增加文件

---
+++ template/src/components/box/toast.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+  import {mapGetters} from 'vuex';
+  export default {
+    name: 'boxToast',
+    computed: mapGetters([]),
+    data() {
+      return {};
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
---
+++ template/src/entry/sample/sample.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    sample
+  </div>
+</template>
+
+<script>
+  import {mapGetters, mapActions} from 'vuex';
+  export default {
+    name: 'sample',
+    components: {},
+    data(){
+      return {};
+    },
+    mounted () {
+    },
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
